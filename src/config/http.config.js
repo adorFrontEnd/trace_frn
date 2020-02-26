4c03ec01575a505fd318fe4029622bcd833d43b0
masecho <masecho@163.com>
2020-02-26 18:34:59 +0800

    修改配置

---
+++ src/config/http.config.js
@@ -0,0 +1,27 @@
+let domain = '';
+
+// 博文
+
+// domain = "http://47.103.71.160:7200";
+
+//正式服接口
+// domain = "https://gw.adorsmart.com/traceUserAdmin";
+domain = "http://47.103.71.160:7200";
+
+let uploadDomain = "http://47.103.71.160:7200";
+let apiUrlPrefix = domain + "/";
+let uploadApiUrlPrefix = uploadDomain + "/";
+let picUrlPrefix = "http://ador-babycar.oss-cn-hangzhou.aliyuncs.com";
+let signKey = "94a7cbbf8511a288d22d4cf8705d61d0";
+let commonSign = '561wd03kkr86615s1de3x45s1d';
+let qrcodeSign = '00461do1156916w1141c56r2ggw2';
+
+export {
+  apiUrlPrefix,
+  uploadApiUrlPrefix,
+  picUrlPrefix,
+  domain,
+  signKey,
+  commonSign,
+  qrcodeSign
+}
